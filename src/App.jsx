96039697489c0e31b82edbae7d69c35a990d3223
Vairamuthu A <vairamuthu1163@gmail.com>
2025-11-22 15:59:34 +0530

    feat: deploy the application using gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ const App = () => {
 
       <div className="wrapper">
         <header>
-          <img src="/hero.png" alt="Hero Banner" />
+          <img src="hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without Hassle
